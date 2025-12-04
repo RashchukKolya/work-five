2c18d240e983338e2746bbd1210abb3f6184c8ce
Nick Rashchuk <raschyk1993@gmail.com>
2025-12-04 23:40:54 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// import axios from "axios";
+import axios from "axios";
 
-// axios.defaults.headers.common["x-api-key"] = "live_XmUynI2y6mRuhD8Zi7A1RaXvJ8ax4IIpg3GS9kszL5S0GlTxMHUR821pEwiqMkZb";
+axios.defaults.headers.common["x-api-key"] = "live_XmUynI2y6mRuhD8Zi7A1RaXvJ8ax4IIpg3GS9kszL5S0GlTxMHUR821pEwiqMkZb";
