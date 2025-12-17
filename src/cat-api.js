5c7e3968ce11a1ed864d52f0d3b59826defa65f3
Nick Rashchuk <raschyk1993@gmail.com>
2025-12-17 09:08:37 +0200

    main task done

---
+++ src/cat-api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_XmUynI2y6mRuhD8Zi7A1RaXvJ8ax4IIpg3GS9kszL5S0GlTxMHUR821pEwiqMkZb";
+
+// https://api.thecatapi.com/v1/breeds'
+// https://api.thecatapi.com/v1/images/search
+
+function fetchBreeds() {
+  return axios.get('https://api.thecatapi.com/v1/breeds')
+    .then((res) => {
+      return res;
+    })
+}
+
+function fetchCatByBreed(breedId) {
+  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((res) => {
+      return res;
+    })
+}
+
+
+export { fetchBreeds, fetchCatByBreed};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
-axios.defaults.headers.common["x-api-key"] = "live_XmUynI2y6mRuhD8Zi7A1RaXvJ8ax4IIpg3GS9kszL5S0GlTxMHUR821pEwiqMkZb";
+const select = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+
+fetchBreeds().then(res => {
+  let optionsList = res.data.reduce((acc, el) => {
+    return acc += `<option value="${el.id}">${el.name}</option>`}, '');
+  select.insertAdjacentHTML('beforeend', optionsList);
+  select.hidden = false;
+})
+  .catch((err) => {
+    error.hidden = false;
+    console.error(err);
+  })
+  .finally(() => {
+  loader.hidden = true;
+});
+
+select.addEventListener('change', (e) => {
+  error.hidden = true;
+  loader.hidden = false;
+  catInfo.classList.add('hidden');
+  let selectedBreed = e.target.value;
+  fetchCatByBreed(selectedBreed).then(res => breedMarkup(res.data[0]))
+    .catch((err) => {
+      error.hidden = false;
+      console.error(err);
+    })
+    .finally(() => {
+    loader.hidden = true;
+  })
+})
+
+function breedMarkup(data) {
+  let { url, breeds: [{ name, temperament, description }] } = data;
+  let markup = `<img src="${url}" class="cat-img" alt="catImg" width="400">
+      <div>
+        <h1>${name}</h1>
+        <p>${description}</p>
+        <p><span>Temperament: </span>${temperament}</p>
+      </div>`
+  catInfo.innerHTML = markup;
+  catInfo.classList.remove('hidden');
+}
